955c6697397bac48563357ec3f5a0432aa87e118
Ahukko8 <ahukko88@gmail.com>
2024-09-07 23:14:06 +0500

    data handling error

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,30 +1,68 @@
+// import { cache } from "@/lib/cache";
+// import db from "@/src/db/db";
+// import ProductsClient from "./ProductsClient";
+
+// // Fetch categories
+// export const getCategories = cache(async () => {
+//   return db.category.findMany({
+//     orderBy: { name: "asc" },
+//   });
+// }, ["/products", "getCategories"]);
+
+// // Fetch products based on category
+// export const getProducts = cache(
+//   async (categoryId?: string) => {
+//     return db.product.findMany({
+//       where: {
+//         isAvailableForPurchase: true,
+//         ...(categoryId && { categoryId }),
+//       },
+//       include: {
+//         category: true,
+//       },
+//       orderBy: { name: "asc" },
+//     });
+//   },
+//   ["/products", "getProducts"]
+// );
+
+// export default async function ProductsPage() {
+//   const categories = await getCategories();
+//   const initialProducts =
+//     categories.length > 0 ? await getProducts(categories[0]?.id) : [];
+
+//   return (
+//     <ProductsClient
+//       initialCategories={categories}
+//       initialProducts={initialProducts}
+//     />
+//   );
+// }
+
 import { cache } from "@/lib/cache";
 import db from "@/src/db/db";
 import ProductsClient from "./ProductsClient";
 
-// Fetch categories
-export const getCategories = cache(async () => {
+// Cache fetching categories
+const getCategories = cache(async () => {
   return db.category.findMany({
     orderBy: { name: "asc" },
   });
 }, ["/products", "getCategories"]);
 
-// Fetch products based on category
-export const getProducts = cache(
-  async (categoryId?: string) => {
-    return db.product.findMany({
-      where: {
-        isAvailableForPurchase: true,
-        ...(categoryId && { categoryId }),
-      },
-      include: {
-        category: true,
-      },
-      orderBy: { name: "asc" },
-    });
-  },
-  ["/products", "getProducts"]
-);
+// Cache fetching products based on category
+const getProducts = cache(async (categoryId?: string) => {
+  return db.product.findMany({
+    where: {
+      isAvailableForPurchase: true,
+      ...(categoryId && { categoryId }),
+    },
+    include: {
+      category: true,
+    },
+    orderBy: { name: "asc" },
+  });
+}, ["/products", "getProducts"]);
 
 export default async function ProductsPage() {
   const categories = await getCategories();

b3615f03808ede0a050dc80c3efd5ac7b457f87f
Ahukko8 <ahukko88@gmail.com>
2024-09-08 14:19:38 +0500

    orders edited

--- src/app/actions/orders.ts
+++ src/app/actions/orders.ts
@@ -5,7 +5,6 @@ import db from "@/src/db/db";
 export async function userOrderExists(email: string, productId: string) {
   return (
     (await db.order.findFirst({
-      where: { user: { email }, productId },
       select: { id: true },
     })) != null
   );

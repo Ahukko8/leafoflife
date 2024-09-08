923abc12b3d6f56b13a71c645cb9146bbe179bae
Ahukko8 <ahukko88@gmail.com>
2024-09-08 20:28:49 +0500

    templateremoved

--- src/app/admin/template.tsx
+++ src/app/admin/template.tsx
@@ -1,16 +1,16 @@
-"use client"
+// "use client"
 
-import { motion } from "framer";
+// import { motion } from "framer";
 
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.div
-        initial={{y: 20, opacity: 0}}
-        animate={{y:0, opacity: 100}}
-        transition={{type: 'easeIn', duration: 0.5}}
-    >
-        {children}
-    </motion.div>
-  )
+// export default function Template({ children }: { children: React.ReactNode }) {
+//   return (
+//     <motion.div
+//         initial={{y: 20, opacity: 0}}
+//         animate={{y:0, opacity: 100}}
+//         transition={{type: 'easeIn', duration: 0.5}}
+//     >
+//         {children}
+//     </motion.div>
+//   )
   
-}
+// }

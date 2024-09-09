78a814512ed10f6e826747e2b3bbd69812295345
Ahukko8 <ahukko88@gmail.com>
2024-09-09 13:37:05 +0500

    title edited sizes

--- src/components/homePage/Hero.tsx
+++ src/components/homePage/Hero.tsx
@@ -10,13 +10,13 @@ const Hero = () => {
     <section className="relative h-screen flex items-center bg-[url(/img/bg.jpg)] ">
       <div className=" container mx-auto flex flex-col md:flex-row items-center justify-between p-6">
         <div className="p-20 flex-1 text-center md:text-left ">
-          <h2 className="text-green-800 lg:text-2xl md:text-2xl sm:text-xl max-sm:text-xl">
+          <h2 className="text-green-800 lg:text-2xl md:text-2xl sm:text-xl max-sm:text-md">
             Your Herbal Clininc
           </h2>
-          <h1 className="font-bold text-gray-800 leading-tight lg:text-6xl md:text-5xl sm:text-5xl max-sm:text-5xl">
+          <h1 className="font-bold text-gray-800 leading-tight lg:text-6xl md:text-2xl sm:text-1xl max-sm:text-1xl">
             Leaf of Life Clinic
           </h1>
-          <p className="mt-4 text-gray-600 max-w-screen-sm md:text-2xl sm:text-xl max-sm:text-lg leading-relaxed max-sm:leading-normal">
+          <p className="mt-4 text-gray-600 max-w-screen-sm md:text-1xl sm:text-lg max-sm:text-lg leading-relaxed max-sm:leading-normal">
             Located in Ha. Kelaa, where we are dedicated to reviving the
             endangered traditions of holistic, herbal, and prophetic medicine.
             Our mission is to preserve and integrate these time-honored healing

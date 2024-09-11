b014757df8152623668dfa4a5f540711fd33e44e
Ahukko8 <ahukko88@gmail.com>
2024-09-11 20:06:41 +0500

    customerfacing updated

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,52 +18,12 @@ export type ProductCardProps = {
   name: string;
   priceInCents: number;
   description: string;
-  imagePath: string;
+  imagePath?: string | undefined;
   isAvailableForPurchase: boolean;
   createdAt: Date;
   updatedAt: Date;
 };
 
-// export function ProductCard({
-//   id,
-//   name,
-//   priceInCents,
-//   description,
-//   imagePath,
-// }: ProductCardProps) {
-//   return (
-//     <Card className="flex overflow-hidden flex-col shadow-md rounded-lg bg-white/90 backdrop-blur-lg bg-opacity-80 hover:shadow-xl transition-transform duration-300 transform hover:scale-105">
-//       <div className="relative w-full h-40 sm:h-48 md:h-56">
-//         <Image
-//           src={imagePath}
-//           fill
-//           alt={name}
-//           className="object-cover rounded-t-lg"
-//         />
-//       </div>
-//       <CardHeader className="p-4 text-center">
-//         <CardTitle className="text-lg font-bold">{name}</CardTitle>
-//         <CardDescription className="text-sm text-gray-500">
-//           {formatCurrency(priceInCents)}
-//         </CardDescription>
-//       </CardHeader>
-//       <CardContent className="flex-grow p-4">
-//         <p className="line-clamp-3 text-justify text-sm text-gray-700">
-//           {description}
-//         </p>
-//       </CardContent>
-//       <CardFooter className="p-4">
-//         <Button
-//           asChild
-//           size="lg"
-//           className="w-full bg-[#62A83c] hover:bg-[#3E3C37] font-bold text-lg"
-//         >
-//           <Link href={`/products/${id}/purchase`}>ORDER</Link>
-//         </Button>
-//       </CardFooter>
-//     </Card>
-//   );
-// }
 
 export function ProductCard({
   id,
@@ -76,18 +36,8 @@ export function ProductCard({
 
   return (
     <Card className="flex overflow-hidden flex-col shadow-md rounded-lg bg-white/90 backdrop-blur-lg bg-opacity-80 hover:shadow-xl transition-transform duration-300 transform hover:scale-105">
-      {/* <div className="relative w-full h-40 sm:h-48 md:h-56">
-        <Image
-          src={imagePath.startsWith('/') ? imagePath : `/${imagePath}`}
-          alt={name}
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className="object-cover rounded-t-lg"
-          onError={() => setImageError(true)}
-        />
-      </div> */}
       <div className="relative w-full h-40 sm:h-48 md:h-56">
-        {!imageError ? (
+        {imagePath ? (
           <Image
             src={imagePath}
             alt={name}
